c4a516fcd2311b800e55baee0743674939625543
vvansuit <vvansuit@outlook.com>
2025-01-20 16:23:05 -0300

    fix: fixed package-json

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcryptjs');
+const prisma = new PrismaClient();
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Route for testing
+app.get('/', (req, res) => {
+  res.send('You entered in the BUNKER');
+});
+
+// Starting Server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Bunker is Running on PORT ${PORT}`);
+});
