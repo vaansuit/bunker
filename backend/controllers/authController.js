f9de01d3123cb89cf337ab0cb28b340fe8ae95d6
vvansuit <vvansuit@outlook.com>
2025-01-20 17:47:45 -0300

    feat: login routine created in back-end

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const jwt = require('jsonwebtoken');
 
 exports.register = async (req, res) => {
     const { username, email, password, role } = req.body;
@@ -45,3 +46,46 @@ exports.register = async (req, res) => {
         res.status(500).json({message: "Error in register user: ", error: error.message});
     }
 }
+
+exports.login = async (req, res) => {
+    const {email, password} = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "Email AND Password required"});
+    }
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email },
+        });
+    
+        if (!user) {
+            return res.status(404).json({ message: "User not found"});
+        }
+    
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+    
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid password"});
+        }
+    
+        const token = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+                role: user.role,
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: '1d' }
+        );
+    
+        res.status(200).json({
+            message: 'Login Successful',
+            token,
+        });
+    
+    } catch (error) {
+        res.status(500).json({ message: "Login Error: ", error: error.message});    
+    }
+};
+
